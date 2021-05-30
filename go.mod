7d73c69840f653f0bbed3c5247e94449ad584f40
Harim Kim <harimkim@hyundai-autoever.com>
2021-05-30 21:44:37 +0900

    message

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/schema v1.2.0
 	github.com/jinzhu/gorm v1.9.16
 	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
--- handler/vehiclehandler.go
+++ handler/vehiclehandler.go
@@ -2,16 +2,15 @@ package handler
 
 import (
 	pb "OJT/core"
+	"OJT/message"
 	"OJT/model"
 	"context"
-	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -39,14 +38,7 @@ func (h *VehicleModelHandler) VehiclesHandler(w http.ResponseWriter, r *http.Req
 
 
 	query := r.URL.Query()
-	jsonbody, err := json.Marshal(query)
-	filter := pb.VehicleModelFilter{}
-	if err := json.Unmarshal(jsonbody, &filter); err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(filter)
-	fmt.Println(query)
+
 	//전체
 	if len(query) == 0{
 		response, err := c.ListVehicleModel(ctx, &pb.ListVehicleModelRequest{Filter: &pb.VehicleModelFilter{}})
@@ -60,87 +52,32 @@ func (h *VehicleModelHandler) VehiclesHandler(w http.ResponseWriter, r *http.Req
 		}
 		w.Write([]byte(response.String()))
 	}else{
-		//데이터 전처리
-		var pageReq uint64
-		if query.Get("Page") != ""{
-			temp, errReq := strconv.ParseUint(query.Get("Page"),10,64)
-			if errReq != nil{
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			pageReq = temp
-		}
-		var rowPerPage uint64
-		if query.Get("RowPerPage") != ""{
-			temp, errReq := strconv.ParseUint(query.Get("RowPerPage"),10,64)
-			if errReq != nil{
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			rowPerPage = temp
-		}
-
-
-		var sortField uint64
-		if query.Get("SortFiled") != "" {
-			temp, errReq := strconv.ParseUint(query.Get("SortField"),10,64)
-			if errReq != nil{
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			sortField = temp
-		}
-
-		var sortOrder uint64
-		if query.Get("SortOrder") != ""{
-			temp, errReq := strconv.ParseUint(query.Get("SortOrder"),10,64)
-			if errReq != nil{
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			sortOrder = temp
-		}
-		var sortOrders []uint64
-		var sortFields []uint64
-		if query.Get("SortOrders") == ""{
-			sortOrders = nil
-		}else{
-			strArray := strings.Split(query.Get("SortOrders"),",")
-			sortOrders = stringInputConvertToUnintArray(strArray, len(strArray))
-		}
-		if query.Get("SortFields") == ""{
-			sortFields = nil
-		}else{
-			strArray := strings.Split(query.Get("SortFields"),",")
-			stringInputConvertToUnintArray(strArray, len(strArray))
-		}
-		filter := pb.VehicleModelFilter{
-			IDs: strings.Split(query.Get("IDs"),","),
-			Brand: query.Get("Brand"),
-			Brands: strings.Split(query.Get("Brands"), ","),
-			Name: query.Get("Name"),
-			FuelType: query.Get("FuelType"),
-			FuelTypes: strings.Split(query.Get("FuelTypes"),","),
-			Grade: query.Get("Grade"),
-			Grades: strings.Split(query.Get("Grades"),","),
-			Page: pageReq,
-			RowPerPage:  rowPerPage,
-			SortField: sortField,
-			SortFields: sortFields,
-			SortOrder: sortOrder,
-			SortOrders: sortOrders,
-		}
-
-		response, err := c.ListVehicleModel(ctx, &pb.ListVehicleModelRequest{Filter: &filter})
-
+		//filter := pb.VehicleModelFilter{
+		//	IDs: strings.Split(query.Get("IDs"),","),
+		//	Brand: query.Get("Brand"),
+		//	Brands: strings.Split(query.Get("Brands"), ","),
+		//	Name: query.Get("Name"),
+		//	FuelType: query.Get("FuelType"),
+		//	FuelTypes: strings.Split(query.Get("FuelTypes"),","),
+		//	Grade: query.Get("Grade"),
+		//	Grades: strings.Split(query.Get("Grades"),","),
+		//}
+		//
+		//response, err := c.ListVehicleModel(ctx, &pb.ListVehicleModelRequest{Filter: &filter})
+		filter2 := message.Decoder(r)
+		//fmt.Println("filter1:",filter)
+		//fmt.Println("filter2:",filter2)
+		response2, err := c.ListVehicleModel(ctx, &pb.ListVehicleModelRequest{Filter: &filter2})
+		//fmt.Println("message using response",response2.String())
 		// log 저장
 		if err != nil {
-			h.CoreDB.Create(&model.Log{APIName:"ListVehicleModel",APICallTime:time.Now(),APISuccess:"FAIL", APIResponseName:response.String()})
+			h.CoreDB.Create(&model.Log{APIName:"ListVehicleModel",APICallTime:time.Now(),APISuccess:"FAIL", APIResponseName:response2.String()})
 			log.Fatalf("could not request: %v", err)
 		}else{
-			h.CoreDB.Create(&model.Log{APIName:"ListVehicleModel",APICallTime:time.Now(),APISuccess:"SUCCESS", APIResponseName:response.String()})
+			fmt.Println("creating log",response2.String())
+			h.CoreDB.Create(&model.Log{APIName:"ListVehicleModel",APICallTime:time.Now(),APISuccess:"SUCCESS", APIResponseName:response2.String()})
 		}
-		w.Write([]byte(response.String()))
+		w.Write([]byte(response2.String()))
 	}
 
 	//h.CoreDB.Commit()
